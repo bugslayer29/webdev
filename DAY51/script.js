e6bb2df55f0149fa511fce8139483a09356c17ca
bugslayer29 <rohitprem2003kr@gmail.com>
2024-05-06 20:35:53 +0530

    day51 added

---
+++ DAY51/script.js
@@ -0,0 +1,13 @@
+
+alert("hello world")   
+// It will show alert inside web browser.
+
+console.log("code  is running")
+// code  is running -- this message will show inside web browser (console when you inspct it)
+
+var a = prompt("enter your number")
+console.log("your name is " + a);
+
+// it will ask you to input a number first then it will print it inside the console.
+
+document.title = "hey i am the title of this document"
